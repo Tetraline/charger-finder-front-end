14e3b8bac616d6da3594749824340c07ae4a03b4
Tetraline <danielsamways@gmail.com>
2022-12-12 23:00:48 +0000

    ensure that keys are always unique

--- src/containers/ResultList/ResultList.jsx
+++ src/containers/ResultList/ResultList.jsx
@@ -3,8 +3,8 @@ import Card from "../../components/Card/Card";
 const ResultList = ({ results }) => {
   const resultsJSX =
     results.length &&
-    results.map((result) => (
-      <Card key={result.ChargeDeviceLocation.Latitude} result={result} />
+    results.map((result, i) => (
+      <Card key={i + result.ChargeDeviceName} result={result} />
     ));
 
   return <>{resultsJSX}</>;
